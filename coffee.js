ee8ad0d5335bbf6f202048a61400b271aa82827d
momapu <momapu@mail.ru>
2020-04-20 19:05:52 +0300

    Машина кушает деньги, но кофе не готовит

---
+++ coffee.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"uses strict";
+// alert( "coffee.js подключён" );
+
+
+function buyCoffee( name, price, element ) {
+//console.log( [name,price,element]);
+
+  let balanceInput = document.querySelector("input[placeholder='Баланс']");
+  
+  if ( +balanceInput.value < price ) {
+    changeDisplayText( "недостаточно средств" );
+    balanceInput.style.border = "2px solid red";
+  } else {
+    balanceInput.value -= price; // Используем напрямую, без переменной
+    balanceInput.style.border = "";
+    cookCoffee( name, element );
+  }
+}
+
+function cookCoffee( name, buttonElement ) {
+  changeDisplayText( " Ваш " + name + " готовится" );
+  let progressBar = document.querySelector( ".progress-bar" );
+  console.log( progressBar );
+}
+
+function changeDisplayText( text ) {
+  let displayText = document.querySelector( '.display-text' );
+  displayText.innerHTML = text;
+}
+
+
+
+
+
